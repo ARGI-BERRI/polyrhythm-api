3dfe989f83bb9ef1954e64f85968d8d00a7faf1e
ARGI-BERRI <me@endroit.net>
2023-10-12 22:44:19 +0900

    chore: prettier

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@
 import { Env } from "./env";
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response("Hello World!");
+  },
 };
